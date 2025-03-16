522295be2b46e47f5b55e7505b1540ea61b66338
jatingoyal <jatin.goyal@exotel.com>
2025-03-17 04:42:28 +0530

    [updated] ModelMapper

--- src/main/java/com/jatingoyal/project/uber/uberApp/configs/MapperConfig.java
+++ src/main/java/com/jatingoyal/project/uber/uberApp/configs/MapperConfig.java
@@ -13,22 +13,6 @@ public class MapperConfig {
     @Bean
     public ModelMapper modelMapper() {
         ModelMapper mapper = new ModelMapper();
-
-        mapper.typeMap(PointDto.class, Point.class).setConverter(context -> {
-            PointDto pointDto = context.getSource();
-            return GeometryUtil.createPoint(pointDto);
-        });
-
-        mapper.typeMap(Point.class, PointDto.class).setConverter(context -> {
-            Point point = context.getSource();
-            double coordinates[] = {
-                    point.getX(),
-                    point.getY()
-            };
-            return new PointDto(coordinates);
-        });
-
-
         return mapper;
     }
 }
