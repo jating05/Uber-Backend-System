58a1a2425a94755c9675395720c5afb9457adf3d
jatingoyal <jatin.goyal@exotel.com>
2025-03-24 13:21:50 +0530

    audit service class

---
+++ src/main/java/com/jatingoyal/project/uber/uberApp/services/AuditLogService.java
@@ -0,0 +1,34 @@
+package com.jatingoyal.project.uber.uberApp.services;
+
+
+import com.jatingoyal.project.uber.uberApp.entities.AuditLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuditLogService {
+
+    private final AuditLogRepository auditLogRepository;
+
+    public void log(String action, String entityType, Long entityId, String details) {
+        String username = "system";
+
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && auth.isAuthenticated()) {
+            username = auth.getName();
+        }
+
+        AuditLog log = AuditLog.builder()
+                .user(username)
+                .action(action)
+                .entityType(entityType)
+                .entityId(entityId)
+                .details(details)
+                .build();
+
+        auditLogRepository.save(log);
+    }
+}
